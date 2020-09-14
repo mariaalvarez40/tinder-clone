b3267575306fefff316d3eebadc91597e1c448ae
mariaalvarez40 <malvarez@rightpoint.com>
2020-09-14 16:39:56 -0500

    clean up file structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Let's Build the Tinder Clone App</h1>
+     <Header/>
+
+     {/* Tinder Cards */}
+     {/* Buttons below tinder cards */}
+
+     {/* Chat Screen  */}
+     {/* Individual chat screen */}
+
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PersonIcon from '@material-ui/icons/Person';
+
+function Header(){
+
+  return(
+    <div>
+      <PersonIcon />
+      <h2>I am a header</h2>
+    </div>
+  );
+}
+export default Header;
