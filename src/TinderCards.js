5163b88bf9acd6fe9cc021e4046ae5a09533aa83
mariaalvarez40 <malvarez@rightpoint.com>
2020-09-28 12:27:44 -0500

    add firebase logic

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,49 +1,54 @@
 //rfce syntax to create component 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TinderCard from 'react-tinder-card';
+import database from './firebase.js';
 import './TinderCards.css';
 
 
+
+
 function TinderCards() {
-  // eslint-disable-next-line
-  const [people, setPeople] = useState([
-    {
-      name: "test",
-      url: "http://via.placeholder.com/640x360", 
-    },
-    {
-      name: "alvarez",
-      url: "http://via.placeholder.com/640x360",
-    },
-  ]);
+
+  const [people, setPeople] = useState([]);
+  
+  //onShapshot: takes a picture of database, returns a snapchot of all documents, always listening or updating
+  //this will run ONCE when the component loads and never again. If brackets are left blank.
+  useEffect(() => {
+    const unsubscribe = database
+    .collection("people")
+    .onSnapshot((snapshot) =>
+      setPeople(snapshot.docs.map((doc) => doc.data()))
+    );
+    return () => {
+      //clean up to listener
+      unsubscribe();
+    };
+  },[]);
+
 
   return (
     <div>
-      <h1>Tinder Cards</h1>
      <div className="tinderCards__cardContainer">
-        {people.map((person) => (
+        {people.map((person, key) => (  
           <TinderCard
+            key={key} 
             //pass in parameters to disable
             preventSwipe={["up", "down"]}
-            className="swipe"
-            //use keys, for lists, allows react to efficiently re-render a list, supports faster reload
-            key={person.name}
+            className="swipe" 
           >
-            <div
+          <div
               className="card"
-              //line 34 use backticks on string interpolation  
+              //line 34 use backticks - string interpolation  
               style={{ backgroundImage: `url("${person.url}")` }}>
-              <h3>{person.name}</h3>
-
-
-            </div>
+          <h3>{person.name}</h3>
+          </div>
           </TinderCard>
         ))}
-     </div>
-      
+     </div>     
     </div>
   );
 }
+
 
 export default TinderCards;
 
